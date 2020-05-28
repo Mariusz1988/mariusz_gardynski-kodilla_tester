13f1b3a6c1669d9d50be024feccfe142150568cf
Mariusz Gardyński <mariuszgardynski@wp.pl>
2020-05-28 15:25:22 +0200

    Initial version

--- kodilla-intro/src/main/java/HelloWorld.java
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -1,8 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Mariusz");
-        System.out.println(31);
-        System.out.println('G');
+         System.out.println("Hello World!");
+         System.out.println("Mariusz");
+         System.out.println(31);
+         System.out.println('G');
     }
 }
+
