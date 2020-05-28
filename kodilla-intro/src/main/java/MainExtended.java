8cf9042ef6dcbc3983ede1f0da054db6c57452ac
Mariusz Gardyński <mariuszgardynski@wp.pl>
2020-05-28 15:10:40 +0200

    Initial version

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args){
+        String carOwner = "Me:D";
+        String carBrand = "MercedezBenz";
+        char carClass ='C';
+        int carYear = 2001;
+        double engineCapacity = 2.2;
+        boolean isGodCar = true;
+        System.out.println(carOwner);
+        System.out.println(carBrand);
+        System.out.println(carClass);
+        System.out.println(carYear);
+        System.out.println(engineCapacity);
+        System.out.println(isGodCar);
+    }
+}
